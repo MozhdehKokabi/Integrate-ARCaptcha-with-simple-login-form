e599c4eb99d6292a8b798e829c19ad460581b13d
Mozhdeh Kokabi <kokabi.mozhdeh@gmail.com>
2023-07-16 16:38:25 +0330

    Captcha verification

---
+++ go.mod
@@ -0,0 +1,5 @@
+module HTMLFRONTR
+
+go 1.20
+
+require github.com/arcaptcha/arcaptcha-go v1.2.0
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/arcaptcha/arcaptcha-go"
 )
 
 func abc(w http.ResponseWriter, r *http.Request) {
@@ -12,11 +14,22 @@ func abc(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	website := arcaptcha.NewWebsite("1pr8g5l057", "66sw65442gwh0wummxvk")
+	challenge_id := r.FormValue("arcaptcha-token")
+	result, err := website.Verify(challenge_id)
+	if err != nil {
+		// throw Error
+		return
+	}
+
 	switch r.Method {
 	case "GET":
 		http.ServeFile(w, r, "./Html-frontend.html")
 
 	case "POST":
+		if !result.Success {
+			http.ServeFile(w, r, "./Html-frontend.html")
+		}
 		if err := r.ParseForm(); err != nil {
 			fmt.Fprintf(w, "parseForm() err: %v\n", err)
 			return
@@ -25,7 +38,6 @@ func abc(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(w, "post form website r.postform = %v\n", r.PostForm)
 		username := r.FormValue("username")
 		password := r.FormValue("password")
-
 		fmt.Fprintf(w, "username=%s\n", username)
 		fmt.Fprintf(w, "password=%s\n", password)
 
@@ -33,6 +45,7 @@ func abc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "only get and post")
 	}
 }
+
 func main() {
 	http.HandleFunc("/", abc)
 
