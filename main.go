48ffc2be3e66bb1c85beb5a21f60a08b5d577a04
Mozhdeh Kokabi <kokabi.mozhdeh@gmail.com>
2023-07-16 15:36:40 +0330

    first commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func abc(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "404 page not found", http.StatusNotFound)
+		return
+	}
+
+	switch r.Method {
+	case "GET":
+		http.ServeFile(w, r, "./Html-frontend.html")
+
+	case "POST":
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "parseForm() err: %v\n", err)
+			return
+		}
+
+		fmt.Println(w, "post form website r.postform = %v\n", r.PostForm)
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		fmt.Fprintf(w, "username=%s\n", username)
+		fmt.Fprintf(w, "password=%s\n", password)
+
+	default:
+		fmt.Fprint(w, "only get and post")
+	}
+}
+func main() {
+	http.HandleFunc("/", abc)
+
+	fmt.Printf("starting server got testing\n")
+	if err := http.ListenAndServe(":8087", nil); err != nil {
+		log.Fatal(err)
+
+	}
+
+}
